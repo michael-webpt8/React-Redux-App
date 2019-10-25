d306926c038eed25434a64aecc4cf517aeaef416
Michael Williams <manicdesigner@gmail.com>
2019-10-25 17:43:20 -0400

    mapStateToProps, progress bar till loads data

--- studio-ghibli/src/App.js
+++ studio-ghibli/src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import './App.scss';
-
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { connect } from 'react-redux';
 import { fetchFilms } from './actions/films';
+import FilmCard from './FilmCard';
 
 function App(props) {
   const { fetchFilms } = props;
@@ -16,6 +18,14 @@ function App(props) {
       <CssBaseline />
       <div className='App bg-sky'>
         <h1>Studio Ghibli</h1>
+
+        {props.error && <p>Error: {props.error}</p>}
+
+        {props.isLoading ? (
+          <CircularProgress color='secondary' />
+        ) : (
+          props.films.map(film => <FilmCard film={film} key={film.id} />)
+        )}
       </div>
     </>
   );
@@ -23,9 +33,9 @@ function App(props) {
 
 const mapStateToProps = state => {
   return {
-    isLoading: state.isLoading,
-    films: state.films,
-    error: state.error
+    isLoading: state.films.isLoading,
+    films: state.films.films,
+    error: state.films.error
   };
 };
 
