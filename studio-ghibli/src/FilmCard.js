69a9feeb24e67cc1e55e8cef28078a7ae0ac9a84
Michael Williams <manicdesigner@gmail.com>
2019-10-25 17:43:38 -0400

    test data card

--- studio-ghibli/src/FilmCard.js
+++ studio-ghibli/src/FilmCard.js
@@ -1,6 +1,32 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+import styled from 'styled-components';
 
 function FilmCard(props) {
-  return <></>;
+  const { title, release_date, description, rt_score } = props.film;
+  console.log('FILMCARD', props.film);
+  return (
+    <>
+      <FilmCards>
+        <CardContent>
+          <Typography color='secondary' gutterBottom>
+            Released: {release_date}
+          </Typography>
+          <Typography variant='h3' component='h2'>
+            {title}
+          </Typography>
+        </CardContent>
+      </FilmCards>
+    </>
+  );
 }
 export default FilmCard;
+
+const FilmCards = styled(Card)`
+  min-width: 275px;
+  max-width: 500px;
+`;
